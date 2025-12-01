4884906dea3e315043748ee034eb769ab7c000e1
akhmadtaufik <akhmadtaufik1703@gmail.com>
2025-12-01 13:25:11 +0700

    feat[backend]: export admin router in router package - register admin router for privileged operations - make admin endpoints available for mounting in main app

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -4,3 +4,4 @@ from .categories import router as categories_router
 from .transactions import router as transactions_router
 from .analytics import router as analytics_router
 from .reports import router as reports_router
+from .admin import router as admin_router
