fad8bcc187fb4673e67cd11c4491b4bf9291e128
akhmadtaufik <akhmadtaufik1703@gmail.com>
2025-12-01 17:14:04 +0700

    chore(frontend): add ESLint and Prettier configuration - Added Vue3 ESLint ruleset and project-specific overrides - Configured Prettier formatting rules for consistency - Enabled modern parser options and environment settings

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+  }
+}
